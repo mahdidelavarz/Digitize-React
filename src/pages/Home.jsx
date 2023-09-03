9d08aefd4826689b85a100bb27f3383ba0835ba9
mahdi delavar <mdelever77@gmail.com>
2023-09-03 12:44:56 +0330

    home style updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -35,147 +35,53 @@ const Home = () => {
           modules={[Autoplay, Pagination]}
           className="mySwiper md:rounded-md md:mt-10"
         >
-          <SwiperSlide><img className="w-full h-28 md:h-auto" src="https://dkstatics-public.digikala.com/digikala-adservice-banners/210c6957708eb21ba69eb79f76875e351b39a433_1691397339.jpg?x-oss-process=image/quality,q_95/format,webp" alt="banner" /></SwiperSlide>
-          <SwiperSlide><img className="w-full h-28 md:h-auto" src="https://dkstatics-public.digikala.com/digikala-adservice-banners/98d0c52c68daa732ea2a34b345fec80322654476_1691572692.jpg?x-oss-process=image/quality,q_95/format,webp" alt="banner" /></SwiperSlide>
-          <SwiperSlide><img className="w-full h-28 md:h-auto" src="https://dkstatics-public.digikala.com/digikala-adservice-banners/e1476e0e049495cbfc74fee2175985e60f6ad688_1691258457.jpg?x-oss-process=image/quality,q_95/format,webp" alt="banner" /></SwiperSlide>
+          <SwiperSlide><img className="w-full h-40 md:h-auto object-cover" src="https://dkstatics-public.digikala.com/digikala-adservice-banners/210c6957708eb21ba69eb79f76875e351b39a433_1691397339.jpg?x-oss-process=image/quality,q_95/format,webp" alt="banner" /></SwiperSlide>
+          <SwiperSlide><img className="w-full h-40 md:h-auto object-cover" src="https://dkstatics-public.digikala.com/digikala-adservice-banners/98d0c52c68daa732ea2a34b345fec80322654476_1691572692.jpg?x-oss-process=image/quality,q_95/format,webp" alt="banner" /></SwiperSlide>
+          <SwiperSlide><img className="w-full h-40 md:h-auto object-cover" src="https://dkstatics-public.digikala.com/digikala-adservice-banners/e1476e0e049495cbfc74fee2175985e60f6ad688_1691258457.jpg?x-oss-process=image/quality,q_95/format,webp" alt="banner" /></SwiperSlide>
         </Swiper>
         <div className="w-full h-auto mt-16 grid grid-cols-12 gap-y-10 gap-x-6">
           <div className="col-span-6 cursor-pointer lg:col-span-3 flex flex-col justify-center items-center">
             <NavLink to={"/phone"} className='flex flex-col items-center'>
-              <div className="w-44 h-44  bg-slate-300 ring-2 ring-rose-500 rounded-full relative ">
-                <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute top-1 right-0'>
-                  <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/JgyxY2bk" alt="phone" />
+              <div className="w-40 h-40 relative bg-slate-50  ring-2 ring-slate-600 flex justify-center items-center rounded-full shadow-xl shadow-slate-500 ">
+                <div className='w-20 h-20 absolute rounded-full -top-3 -right-3 bg-white ring-2 ring-slate-500'>
+                  <img src="https://my.uupload.ir/dl/kjgY1v7e" alt="phone" />
                 </div>
-                <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute top-1 left-0'>
-                  <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/kjgY1v7e" alt="phone" />
-                </div>
-                <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute top-12 right-12'>
-                  <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/VX71EJ6p" alt="phone" />
-                </div>
-                <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute bottom-1 right-0'>
-                  <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/JgyxY2bk" alt="phone" />
-                </div>
-                <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute bottom-1 left-0'>
-                  <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/zbRgvXJB" alt="phone" />
-                </div>
+                <img src="https://my.uupload.ir/dl/JgyxY2bk" alt="phone" className='object-cover' />
               </div>
               <span className="text-xs md:text-sm  mt-2">گوشی موبایل</span>
             </NavLink>
           </div>
           <div className="col-span-6 cursor-pointer lg:col-span-3 flex flex-col justify-center items-center">
             <NavLink to={"/laptop"} className='flex flex-col items-center'>
-              <div className="w-44 h-44   bg-slate-300 ring-2 ring-rose-500  rounded-full relative">
-
-                <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute top-1 right-0'>
-                  <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/n2vYGpvD" alt="phone" />
+              <div className="w-40 h-40 relative  bg-slate-50  ring-2 ring-slate-600 flex justify-center items-center rounded-full shadow-xl shadow-slate-500 ">
+                <div className='w-20 h-20 absolute rounded-full -top-3 -right-3 bg-white ring-2 ring-slate-500'>
+                  <img src="https://my.uupload.ir/dl/wZ21EgZB" alt="phone"/>
                 </div>
-                <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute top-1 left-0'>
-                  <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/n2vL9Xjn" alt="phone" />
-                </div>
-                <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute top-12 right-12'>
-                  <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/wZ21ENzw" alt="phone" />
-                </div>
-                <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute bottom-1 right-0'>
-                  <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/mbGo1N7b" alt="phone" />
-                </div>
-                <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute bottom-1 left-0'>
-                  <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/EOgBrpWp" alt="phone" />
-                </div>
-
+                <img className='object-cover' src="https://my.uupload.ir/dl/EOgBrpWp" alt="laptop" />
               </div>
               <span className="text-xs md:text-sm  mt-2">لپ تاپ</span>
             </NavLink>
           </div>
           <div className="col-span-6 cursor-pointer lg:col-span-3 flex flex-col justify-center items-center">
             <NavLink to={"/watch"} className='flex flex-col items-center'>
-              <div className="w-44 h-44   bg-slate-300 ring-2 ring-rose-500  rounded-full relative">
-
-                <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute top-1 right-0'>
-                  <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/yoO19j11" alt="phone" />
+              <div className="w-40 h-40 relative  bg-slate-50  ring-2 ring-slate-600 flex justify-center items-center rounded-full shadow-xl shadow-slate-500 ">
+                <div className='w-20 h-20 absolute rounded-full -top-3 -right-3 bg-white ring-2 ring-slate-500'>
+                  <img src="https://my.uupload.ir/dl/EOgen710" alt="" />
                 </div>
-                <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute top-1 left-0'>
-                  <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/1LzNYWjb" alt="phone" />
-                </div>
-                <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute top-12 right-12'>
-                  <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/JgyprBBB" alt="phone" />
-                </div>
-                <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute bottom-1 right-0'>
-                  <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/wZ2gOLGw" alt="phone" />
-                </div>
-                <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute bottom-1 left-0'>
-                  <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/5L5jga4e" alt="phone" />
-                </div>
-
+                <img className=' object-cover' src="https://my.uupload.ir/dl/5L5jga4e" alt="watch" />
               </div>
               <span className="text-xs md:text-sm  mt-2">ساعت هوشمند</span>
             </NavLink>
           </div>
           <div className="col-span-6 cursor-pointer lg:col-span-3 flex flex-col justify-center items-center">
-            <div className="w-44 h-44   bg-slate-300 ring-2 ring-rose-500  rounded-full relative">
-
-              <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute top-1 right-0'>
-                <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/JgyxY2bk" alt="phone" />
+            <div className="w-40 h-40  bg-slate-50 relative  ring-2 ring-slate-600 flex justify-center items-center rounded-full shadow-xl shadow-slate-500 ">
+              <div className='w-20 h-20 absolute rounded-full -top-3 -right-3 bg-white ring-2 ring-slate-500'>
+                <img src="https://my.uupload.ir/dl/v9pXkEGb" alt="" />
               </div>
-              <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute top-1 left-0'>
-                <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/kjgY1v7e" alt="phone" />
-              </div>
-              <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute top-12 right-12'>
-                <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/VX71EJ6p" alt="phone" />
-              </div>
-              <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute bottom-1 right-0'>
-                <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/JgyxY2bk" alt="phone" />
-              </div>
-              <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute bottom-1 left-0'>
-                <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/zbRgvXJB" alt="phone" />
-              </div>
-
+              <img className='object-contain w-40 h-40' src="https://my.uupload.ir/dl/4e5KM0e2" alt="phone" />
             </div>
             <span className="text-xs md:text-sm  mt-2">هدفون و هندزفری</span>
           </div>
-          {/* <div className="col-span-6 cursor-pointer lg:col-span-2 flex flex-col justify-center items-center">
-            <div className="w-44 h-44   bg-slate-300 ring-2 ring-rose-500  rounded-full relative">
-
-              <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute top-1 right-0'>
-                <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/JgyxY2bk" alt="phone" />
-              </div>
-              <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute top-1 left-0'>
-                <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/kjgY1v7e" alt="phone" />
-              </div>
-              <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute top-12 right-12'>
-                <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/VX71EJ6p" alt="phone" />
-              </div>
-              <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute bottom-1 right-0'>
-                <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/JgyxY2bk" alt="phone" />
-              </div>
-              <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute bottom-1 left-0'>
-                <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/zbRgvXJB" alt="phone" />
-              </div>
-
-            </div>
-            <span className="text-xs md:text-sm  mt-2">گیمینگ</span>
-          </div> */}
-          {/* <div className="col-span-6 cursor-pointer lg:col-span-2 flex flex-col justify-center items-center">
-            <div className="w-44 h-44   bg-slate-300 ring-2 ring-rose-500  rounded-full relative">
-
-              <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute top-1 right-0'>
-                <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/JgyxY2bk" alt="phone" />
-              </div>
-              <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute top-1 left-0'>
-                <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/kjgY1v7e" alt="phone" />
-              </div>
-              <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute top-12 right-12'>
-                <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/VX71EJ6p" alt="phone" />
-              </div>
-              <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute bottom-1 right-0'>
-                <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/JgyxY2bk" alt="phone" />
-              </div>
-              <div className='w-20 h-20 rounded-full ring-2 ring-rose-500 bg-white absolute bottom-1 left-0'>
-                <img className='w-20 h-20 object-contain' src="https://my.uupload.ir/dl/zbRgvXJB" alt="phone" />
-              </div>
-
-            </div>
-            <span className="text-xs md:text-sm  mt-2">لوازم جانبی</span>
-          </div> */}
         </div>
         <div className="col-span-12 h-auto bg-red-500 mt-10 p-x-2 py-6 grid grid-cols-12">
 
